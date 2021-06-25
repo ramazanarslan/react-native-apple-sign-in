053e20603daa5e74fd3b4d01b41284018df28251
mustafakemalelma <mustafakemalelma@gmail.com>
2021-06-25 14:44:39 +0300

    makes it customizable

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-import React from 'react';
-import { NativeModules, requireNativeComponent ,Platform } from 'react-native';
+import { NativeModules } from 'react-native';
 
 const { AppleSignIn } = NativeModules;
 
-export const SignInWithAppleWhiteButton = requireNativeComponent('RNSignInWithAppleWhiteButton');
-
-export const SignInWithAppleBlackButton = requireNativeComponent('RNSignInWithAppleBlackButton');
-
-export const SignInWithAppleButton = (style, callBack, buttonStyle) => {
-    if(Platform.OS === 'ios'){
-        const Button = buttonStyle === "black" ? SignInWithAppleBlackButton : SignInWithAppleWhiteButton;
-        return <Button style={style} onPress={async () => {
-
-            await AppleSignIn.requestAsync({
-                requestedScopes: [AppleSignIn.Scope.FULL_NAME, AppleSignIn.Scope.EMAIL],
-                requestedOperation: AppleSignIn.Operation.LOGIN,
-            }).then((response) => {
-                callBack(response) //Display response
-            }, (error) => {
-                callBack(error) //Display error
-
-            });
-
-        }} />
-    }else{
-        return null
-
-    }
-
+export async function performRequest() {
+    return await AppleSignIn.requestAsync({
+        requestedScopes: [AppleSignIn.Scope.FULL_NAME, AppleSignIn.Scope.EMAIL],
+        requestedOperation: AppleSignIn.Operation.LOGIN,
+    })
 }
 
 export default AppleSignIn;

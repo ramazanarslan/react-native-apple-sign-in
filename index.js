f91130d5f8641c37d8a48ecfe93dbefa9191bdb6
mustafakemalelma <mustafakemalelma@gmail.com>
2021-06-25 14:47:54 +0300

    changes name

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { AppleSignIn } = NativeModules;
 
-export async function performRequest() {
+export async function performAppleSignInRequest() {
+    if (Platform.OS !== "ios") throw new Error("Platform must be IOS");
+
     return await AppleSignIn.requestAsync({
         requestedScopes: [AppleSignIn.Scope.FULL_NAME, AppleSignIn.Scope.EMAIL],
         requestedOperation: AppleSignIn.Operation.LOGIN,

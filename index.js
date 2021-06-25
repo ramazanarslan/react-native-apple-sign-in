82db2f11e735c03fe0ce4406f675f9dca67bf08b
mustafakemalelma <mustafakemalelma@gmail.com>
2021-06-25 16:44:50 +0300

    adds support for old versions

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-import { NativeModules, Platform } from 'react-native';
+import React from 'react';
+import { NativeModules, requireNativeComponent, Platform } from 'react-native';
 
 const { AppleSignIn } = NativeModules;
+
+export const SignInWithAppleWhiteButton = requireNativeComponent('RNSignInWithAppleWhiteButton');
+
+export const SignInWithAppleBlackButton = requireNativeComponent('RNSignInWithAppleBlackButton');
+
+export const SignInWithAppleButton = (style, callBack, buttonStyle) => {
+    if (Platform.OS === 'ios') {
+        const Button = buttonStyle === "black" ? SignInWithAppleBlackButton : SignInWithAppleWhiteButton;
+        return <Button style={style} onPress={async () => {
+
+            await AppleSignIn.requestAsync({
+                requestedScopes: [AppleSignIn.Scope.FULL_NAME, AppleSignIn.Scope.EMAIL],
+                requestedOperation: AppleSignIn.Operation.LOGIN,
+            }).then((response) => {
+                callBack(response) //Display response
+            }, (error) => {
+                callBack(error) //Display error
+
+            });
+
+        }} />
+    } else {
+        return null
+    }
+
+}
 
 export const performAppleSignInRequest = async () => {
     if (Platform.OS !== "ios") throw new Error("Platform must be IOS");
